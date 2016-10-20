4f40d4766ed297d7ede60f9bb2ca3c7a00a9d540
Peter van Dam <petervandam@gmail.com>
2016-10-20 14:40:53 +0200

    Made changes to the variables, the usage text and the options

--- Fo_effector_clustering.py
+++ Fo_effector_clustering.py
@@ -1,25 +1,17 @@
-import os, datetime, sys, subprocess
+import os, datetime
 starttime_unformatted = datetime.datetime.now()
 starttime = datetime.datetime.now().strftime("%y.%m.%d_%Hh%Mm%S")
 from optparse import OptionParser
-#######################################################################
-####[EDIT THESE VARIABLES TO SET STUFF BEFORE RUNNING THE PIPELINE]####
-#######################################################################
-
-# Dependencies:
-# - SignalP
-# - AUGUSTUS
-# - BioPython 
-# - python package "BCBio"
-# - R libraries "gplots" and "ctc" must be installed!
+##########################################################
+####[EDIT THESE VARIABLES BEFORE RUNNING THE PIPELINE]####
+##########################################################
 
 blastdatabasedir			= '/Users/Peter/Documents/Sequences/Fo_genomes/blastdbs'
-contigprefix 				= 'contig_' #contig_
+contigprefix 				= 'contig_' 	# default: contig_
 AUGUSTUS_path 				= '/Users/Peter/Programming/augustus-3.1/bin/augustus'
 AUGUSTUS_CONFIG_path 		= '/Users/Peter/Programming/augustus-3.1/config'
 BLASTbindir 				= '/usr/local/ncbi/blast/bin'
 SignalPpath					= '/Users/Peter/Programming/signalp/signalp-4.1/signalp'
-
 
 #mimp-search variables
 distance_MetStop			= '2500' 		# sequence downstream of motif used for ORF prediction
@@ -31,14 +23,16 @@ SignalP_threshold			= '0.550'
 
 #blast variables
 PERC_IDENTITY_THRESH 		= '30'
-BLAST_task					= 'blastn' #or megablast
-buildblastdb				= 'no'
+BLAST_task					= 'blastn' 		# or megablast
+buildblastdb				= 'yes' 		# should a new blast db be built for the genome files encountered? (default=yes)
+
 #clustering variables:
 distance_matrix_rows		= '1'
 clustering_method_rows		= 'average'
 distance_matrix_cols		= '1'
 clustering_method_cols		= 'average'
 
+# Clustering methods that can be used for the hierarchical clustering:
 #1 = Jaccard index (1901) S3 coefficient of Gower & Legendre s1 = a / (a+b+c)
 #2 = Simple matching coefficient of Sokal & Michener (1958) S4 coefficient of Gower & Legendre s2 = (a+d) / (a+b+c+d)
 #3 = Sokal & Sneath(1963) S5 coefficient of Gower & Legendre s3 = a / (a + 2(b + c))
@@ -50,32 +44,35 @@ clustering_method_cols		= 'average'
 #9 = Phi of Pearson S14 coefficient of Gower & Legendre s9 = (ad - bc) / sqrt((a + b)(a + c)(d + b)(d + c))
 #10 = S2 coefficient of Gower & Legendre S10 = a / (a + b + c + d)
 
-UseNonRedundantEffectors	= False 		#(step C)
 #######################################################################
 
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
-scriptdir = currentdir.split('/')[-1]
-default_outputdir = currentdir.split(scriptdir)[0]+'output_'+starttime
+#scriptdir = currentdir.split('/')[-1]
+default_outputdir = currentdir+'/output/output_'+starttime
 
 
 
-usage = 'python _pipeline_mimpsearch_hcluster.py -i [infile] <options>\n'+'-'*20+'\n\
-This script will take a folder with genome fasta files, find mimps and mimp terminal inverted repeats and try to identify candidate effectors. These will be clustered into families and then \
-BLASTed against each of the genomes to identify presence or absence. These binary patterns will be clustered to produce a clustering figure. \n-Peter van Dam (feb 2016)'
+usage = '\n'+'-'*20+'\npython Fo_effector_clustering.py -i [infolder] <options>\n'+'-'*20+'\n\
+This script will take a folder with genome fasta files, find mimps and mimp terminal inverted repeats \
+and try to identify candidate effectors. These will be clustered into families and then BLASTed against \
+each of the genomes to identify presence (1) or absence (0). These binary patterns will be hierarchically \
+clustered in R to produce a clustering figure using "heatmap.3.R". \n-Peter van Dam (feb 2016)'
 parser = OptionParser(usage=usage)
 
 parser.add_option("-i", "--in", dest="infolder", help="provide a folder with genome fasta files")
-parser.add_option("-o", "--out", dest="outfolder", help="output folder; default is: ../output_+starttime")
-parser.add_option("-e", "--effector_fasta", dest="effector_fasta", help="Skip the effector prediction pipeline and go straight go to blasting and clustering using a self-supplied effector list in fasta format.")
-parser.add_option("-m", "--metstoppredictiononly", dest="MetStopPredictionOnly", action="store_true", default=False,
-					help="Do not use Augustus gene prediction, only first met to first stop.")
+parser.add_option("-o", "--out", dest="outfolder", help="output folder; default is: "+default_outputdir)
+parser.add_option("-e", "--effector_fasta", dest="effector_fasta", help="Skip the effector prediction pipeline and go straight to blasting and clustering using a self-supplied effector list in fasta format.")
+parser.add_option("-m", "--met_stop_prediction_only", dest="met_stop_prediction_only", action="store_true", default=False,
+					help="Do not use Augustus gene prediction, only identify ORFs by looking for first occurrence of a Methionine (met) to first Stop codon (stop).")
+parser.add_option("-u", "--use_nonredundant_effectors", dest="use_nonredundant_effectors", action="store_true", default=False,
+					help="In case multiple ORF prediction methods are used (NOT using -m), the outputs from these methods can be clustered first, then concatenated, then clustered (use this -u option). \
+					Default behaviour is not to use this option, meaning taking the output from both methods, concatenate them and then cluster them into gene families.")
 
 (options, args) = parser.parse_args()
 
 if options.infolder == None:
-	print '\n[ERROR!]\nPlease specify a folder containing Fusarium oxysporum genomes.\n'
-	print '"-i infolder"'
+	print '\n[ERROR!]\nPlease specify a folder containing Fusarium oxysporum genomes: "-i [infolder]"'
 	print 'Use "-h" to see additional options.\n'
 	quit()
 else: 
@@ -97,7 +94,7 @@ if options.effector_fasta == None:
 	cline1a = ' '.join(['python', currentdir+'/01a.mimpfinder_combine_to_putefflist_MetStop.py', genomedir, outputdir, contigprefix, distance_MetStop, min_prot_len, max_prot_len, max_d2m, SignalPpath, SignalP_threshold])
 	print cline1a, os.system(cline1a)
 
-	if options.MetStopPredictionOnly:
+	if options.met_stop_prediction_only:
 		print 'No Augustus gene prediction will be executed.'
 	else:
 		cline1b = ' '.join(['python', currentdir+'/01b.mimpfinder_combine_to_putefflist_AUGUSTUS.py', genomedir, outputdir, contigprefix, AUGUSTUS_path, '--AUGUSTUS_CONFIG_PATH='+AUGUSTUS_CONFIG_path, distance_Augustus, min_prot_len, max_prot_len, max_d2m, SignalPpath, SignalP_threshold])
@@ -111,7 +108,7 @@ if options.effector_fasta == None:
 
 	cline2a = ' '.join(['python', currentdir+'/02.cluster_putefflists.py', all_putative_effectors_MetStop, blastdatabasedir, leave_put_eff_identifiers_during_clustering, BLASTbindir])
 	print cline2a, os.system(cline2a)
-	if options.MetStopPredictionOnly:
+	if options.met_stop_prediction_only:
 		print 'No clustering of Augustus effectors necessary.'
 	else:
 		cline2b = ' '.join(['python', currentdir+'/02.cluster_putefflists.py', all_putative_effectors_Augustus, blastdatabasedir, leave_put_eff_identifiers_during_clustering, BLASTbindir])
@@ -125,13 +122,13 @@ if options.effector_fasta == None:
 		os.makedirs(outputdir+'/02.cluster_putative_effectors/')
 	all_putative_effectors_concatenated = outputdir+'/02.cluster_putative_effectors/all_putative_effectors_concatenated.fasta'
 
-	if options.MetStopPredictionOnly:
-		if UseNonRedundantEffectors == False:
+	if options.met_stop_prediction_only:
+		if options.use_nonredundant_effectors == False:
 			clinecat = 'cat '+all_putative_effectors_MetStop+' > '+all_putative_effectors_concatenated
 		else:
 			clinecat = 'cat '+all_putative_effectors_MetStop_clustered+' > '+all_putative_effectors_concatenated
 	else:
-		if UseNonRedundantEffectors == False:
+		if options.use_nonredundant_effectors == False:
 			clinecat = 'cat '+all_putative_effectors_MetStop+' '+all_putative_effectors_Augustus+' > '+all_putative_effectors_concatenated
 		else:
 			clinecat = 'cat '+all_putative_effectors_MetStop_clustered+' '+all_putative_effectors_Augustus_clustered+' > '+all_putative_effectors_concatenated
